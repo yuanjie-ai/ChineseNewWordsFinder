75f1e69a103d9f6a79ade550d247da540f76586d
尐 <313303303@qq.com>
2018-11-29 20:12:20 +0800

    Update new_words_finder.py

--- new_words_finder/new_words_finder.py
+++ new_words_finder/new_words_finder.py
@@ -7,11 +7,12 @@ __mtime__ = '18-11-29'
 """
 import os
 import jieba
+
 jieba.enable_parallel(4)
 jieba.lcut("分词测试")
 from tqdm import tqdm
 from .trie import TrieNode
-from .utils import load_dictionary, save_model, load_model
+from .utils import get_stopwords, load_dictionary, Ngrams, save_model, load_model, get_module_path
 
 
 class NewWordsFinder(object):
@@ -19,6 +20,9 @@ class NewWordsFinder(object):
     def __init__(self, corpus):
         self.corpus = corpus
         self.root = None
+        self.ng = Ngrams()
+        self.data_dir = get_module_path('./data', __file__)
+        self.stopwords = get_stopwords(self.data_dir + '/stopwords.txt')
         self.__root_init()
         self.__load_data()
 
@@ -26,25 +30,24 @@ class NewWordsFinder(object):
         return self.root.find_word(top_n)[1]  # result, add_words
 
     def __root_init(self):
-        root_name = data_dir + "/root.pkl"
+        root_name = self.data_dir + "/root.pkl"
         if os.path.exists(root_name):
             self.root = load_model(root_name)
         else:
-            dict_name = data_dir + '/dict.txt'
+            dict_name = self.data_dir + '/dict.txt'
             word_freq = load_dictionary(dict_name)
             self.root = TrieNode('*', word_freq)
             save_model(self.root, root_name)
 
     def __load_data(self):
         """插入节点"""
+
         with open(self.corpus) as f:
             for line in tqdm(f, "Loading Corpus ..."):
-                word_list = [x for x in jieba.cut(line.strip(), cut_all=False) if x not in stopwords]
-                ngrams = ng.everygrams(word_list, max_len=3)
+                word_list = [x for x in jieba.cut(line.strip(), cut_all=False) if x not in self.stopwords]
+                ngrams = self.ng.everygrams(word_list, max_len=3)
                 for d in ngrams:
                     self.root.add(d)
-
-
 
 
 if __name__ == '__main__':
