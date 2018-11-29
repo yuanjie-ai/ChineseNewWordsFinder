87aaf0e8616a70a9b0dd92d84053520e21144b12
尐 <313303303@qq.com>
2018-11-29 20:03:50 +0800

    Update new_words_finder.py

--- new_words_finder/new_words_finder.py
+++ new_words_finder/new_words_finder.py
@@ -11,7 +11,8 @@ jieba.enable_parallel(4)
 jieba.lcut("分词测试")
 from tqdm import tqdm
 from .trie import TrieNode
-from .utils import get_stopwords, load_dictionary, Ngrams, save_model, load_model, get_module_path
+from .utils import load_dictionary, save_model, load_model
+
 
 class NewWordsFinder(object):
 
@@ -20,30 +21,26 @@ class NewWordsFinder(object):
         self.root = None
         self.__root_init()
         self.__load_data()
-        self.data_dir = get_module_path('./data', __file__)
-
-        self.ng = Ngrams()
 
     def get_new_words(self, top_n=5):
         return self.root.find_word(top_n)[1]  # result, add_words
 
     def __root_init(self):
-        root_name = self.data_dir + "/root.pkl"
+        root_name = data_dir + "/root.pkl"
         if os.path.exists(root_name):
             self.root = load_model(root_name)
         else:
-            dict_name = self.data_dir + '/dict.txt'
+            dict_name = data_dir + '/dict.txt'
             word_freq = load_dictionary(dict_name)
             self.root = TrieNode('*', word_freq)
             save_model(self.root, root_name)
 
     def __load_data(self):
         """插入节点"""
-        stopwords = get_stopwords(self.data_dir + '/stopwords.txt')
         with open(self.corpus) as f:
             for line in tqdm(f, "Loading Corpus ..."):
                 word_list = [x for x in jieba.cut(line.strip(), cut_all=False) if x not in stopwords]
-                ngrams = self.ng.everygrams(word_list, max_len=3)
+                ngrams = ng.everygrams(word_list, max_len=3)
                 for d in ngrams:
                     self.root.add(d)
 
