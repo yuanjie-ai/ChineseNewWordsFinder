22ec4588f64f68c8c06be475472f41fcd493c96a
尐 <313303303@qq.com>
2018-11-29 20:05:12 +0800

    Update __init__.py

--- new_words_finder/__init__.py
+++ new_words_finder/__init__.py
@@ -6,8 +6,9 @@ __author__ = 'JieYuan'
 __mtime__ = '18-11-29'
 """
 from .utils import get_stopwords, Ngrams, get_module_path
-from .new_words_finder import NewWordsFinder
 
 data_dir = get_module_path('./data', __file__)
 stopwords = get_stopwords(data_dir + '/stopwords.txt')
 ng = Ngrams()
+
+from .new_words_finder import NewWordsFinder
